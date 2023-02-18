58fbfcccbf652d6f48aee5888df4f94564ed300e
Tatianam03 <cm0249042022@unad.edu.sv>
2023-02-18 15:22:15 -0600

    Actividad asincronica

---
+++ Tarea2/Negocios/ClsPalabra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea2.Entidades;
+
+namespace Tarea2.Negocios
+{
+    internal class ClsPalabra
+    {
+     
+        public int contador(Palabra palabra) { return palabra.palabra.Length; }
+    }
+}
---
+++ Tarea2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea2.Entidades;
+using Tarea2.Negocios;
+
+namespace Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra");
+            Palabra palabra = new Palabra();
+            palabra.palabra = Console.ReadLine();
+
+            ClsPalabra contar = new ClsPalabra();
+
+            Console.WriteLine("La palabra " + palabra.palabra + " tiene " + contar.contador(palabra) + " letras");
+            Console.ReadKey();
+        }
+    }
+}
